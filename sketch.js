cd4058720dee6542e827a4ee86c7c8b9ef3378f6
Rishith18 <69175269+Rishith18@users.noreply.github.com>
2020-10-08 20:06:05 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var database;
+var form, game, player;
+var gameState = 0;
+var playerCount=0;
+var allPlayers;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    
+}
